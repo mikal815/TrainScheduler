e756314be30ee4754fd7df42d39abfd2a46027a2
Mike M <mikal815@users.noreply.github.com>
2018-06-27 21:41:07 -0400

    initialized firebase

---
+++ assets/javascript/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+console.log("working");
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBIbgO5NOvtyoZqSR--pkeWpHAfBeKFix8",
+    authDomain: "trainscheduler-18a5d.firebaseapp.com",
+    databaseURL: "https://trainscheduler-18a5d.firebaseio.com",
+    projectId: "trainscheduler-18a5d",
+    storageBucket: "",
+    messagingSenderId: "773159365008"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+
+});
